32b54acf4518315241f0ee33be1e532e3c318366
VitaAin <wtaincb@126.com>
2017-11-28 10:10:58 +0800

    modify API_ROOT

--- src/api/index.js
+++ src/api/index.js
@@ -3,56 +3,71 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
-const API_ROOT = "../../static/data/";
+Vue.http.options.root = process.env.API_ROOT;
+Vue.http.headers.common.Accept = `application/json`;
+
+// Vue.http.interceptors.push((request, next) => {
+//   const auth = store.state.account.auth;
+//   if (auth.check()) {
+//     const accessToken = auth.access_token;
+//     Vue.http.headers.common.Authorization = `Bearer ${accessToken}`;
+//   } else {
+//     delete Vue.http.headers.common.Authorization;
+//   }
+//   next();
+// });
+
+const API_ROOT_TEST = "../../static/data/";
 
 export default {
   getHomeBanners() {
-    return Vue.http.get(API_ROOT + "HomeBanner.json");
+    return Vue.http.get(API_ROOT_TEST + "HomeBanner.json");
   },
   getArticles() {
-    return Vue.http.get(API_ROOT + "Articles.json");
+    // return Vue.http.get(API_ROOT_TEST + "Articles.json");
+    return Vue.http.get("articles");
   },
   getHotArticles() {
-    return Vue.http.get(API_ROOT + "HotArticles.json");
+    return Vue.http.get(API_ROOT_TEST + "HotArticles.json");
   },
   getHotTags() {
-    return Vue.http.get(API_ROOT + "HotTags.json");
+    return Vue.http.get(API_ROOT_TEST + "HotTags.json");
   },
   getArticle(id) {
-    return Vue.http.get(API_ROOT + "OneArticle.json");
+    return Vue.http.get(API_ROOT_TEST + "OneArticle.json");
   },
   getAllTags() {
-    return Vue.http.get(API_ROOT + "AllTags.json");
+    return Vue.http.get(API_ROOT_TEST + "AllTags.json");
   },
   getAllCategories() {
-    return Vue.http.get(API_ROOT + "AllCategories.json");
+    return Vue.http.get(API_ROOT_TEST + "AllCategories.json");
   },
   createArticle(article) {
     // return Vue.http.post(API_ROOT + "");
-    return Vue.http.get(API_ROOT + "EditArticle.json");
+    return Vue.http.get(API_ROOT_TEST + "EditArticle.json");
   },
   editArticle(id, article) {
     // return Vue.http.post(API_ROOT + "");
-    return Vue.http.get(API_ROOT + "EditArticle.json");
+    return Vue.http.get(API_ROOT_TEST + "EditArticle.json");
   },
   login(user) {
     // return Vue.http.post(API_ROOT + "", user);
-    return Vue.http.get(API_ROOT + "Login.json");
+    return Vue.http.get(API_ROOT_TEST + "Login.json");
   },
   logout() {
-    return Vue.http.get(API_ROOT + "Logout.json");
+    return Vue.http.get(API_ROOT_TEST + "Logout.json");
   },
   register(params) {
     // return Vue.http.post(API_ROOT + "", params);
-    return Vue.http.get(API_ROOT + "Register.json");
+    return Vue.http.get(API_ROOT_TEST + "Register.json");
   },
   getUser(id) {
-    return Vue.http.get(API_ROOT + "User.json");
+    return Vue.http.get(API_ROOT_TEST + "User.json");
   },
   getUserArticles(id) {
-    return Vue.http.get(API_ROOT + "UserArticles.json");
+    return Vue.http.get(API_ROOT_TEST + "UserArticles.json");
   },
   getUserReplies(id) {
-    return Vue.http.get(API_ROOT + "UserReplies.json");
+    return Vue.http.get(API_ROOT_TEST + "UserReplies.json");
   }
 }
